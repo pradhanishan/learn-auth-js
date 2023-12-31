de1746b985b56fb09d15120d678e2129c40684c3
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-01-01 01:31:09 +0700

    add register functionality

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod";
 import { registerSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validatedFields = registerSchema.safeParse(values);
@@ -9,5 +12,24 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
-  return { success: "Email sent!" };
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification token email
+
+  return { success: "User created!" };
 };
