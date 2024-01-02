aad63c1a90d8851d66791a35532fa6d22364ef09
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-01-02 21:42:44 +0700

    add signout

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,21 @@
 import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { signOut } from "@/auth";
 
 export default async function Settings() {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div className="p-4">
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button type="submit">Sign out</Button>
+      </form>
+    </div>
+  );
 }
